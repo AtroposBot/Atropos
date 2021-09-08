f30bc02c089a7d7d3b84a6f1ee31d439f88114b8
Laarryy <hey@laarryy.dev>
2021-09-07 22:34:34 -0600

    oop

--- src/main/java/dev/laarryy/Icicle/commands/logging/LogSettingsCommand.java
+++ src/main/java/dev/laarryy/Icicle/commands/logging/LogSettingsCommand.java
@@ -2,7 +2,9 @@ package dev.laarryy.Icicle.commands.logging;
 
 import dev.laarryy.Icicle.commands.Command;
 import discord4j.core.event.domain.interaction.SlashCommandEvent;
+import discord4j.discordjson.json.ApplicationCommandOptionData;
 import discord4j.discordjson.json.ApplicationCommandRequest;
+import discord4j.rest.util.ApplicationCommandOptionType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import reactor.core.publisher.Mono;
@@ -13,6 +15,12 @@ public class LogSettingsCommand implements Command {
     private final ApplicationCommandRequest request = ApplicationCommandRequest.builder()
             .name("logsettings")
             .description("Modify logging settings.")
+            .addOption(ApplicationCommandOptionData.builder()
+                    .name("setlogchannel")
+                    .description("Generate a random number?")
+                    .type(ApplicationCommandOptionType.BOOLEAN.getValue())
+                    .required(false)
+                    .build())
             .defaultPermission(true)
             .build();
 
