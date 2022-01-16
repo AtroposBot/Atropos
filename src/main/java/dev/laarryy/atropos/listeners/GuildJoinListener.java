9a0602201057259b766a7771912d561fd85c52b7
Laarryy <hey@laarryy.dev>
2022-01-16 03:52:13 -0700

    autorole part 2

--- src/main/java/dev/laarryy/atropos/listeners/GuildJoinListener.java
+++ src/main/java/dev/laarryy/atropos/listeners/GuildJoinListener.java
@@ -1,8 +1,10 @@
 package dev.laarryy.atropos.listeners;
 
 import dev.laarryy.atropos.utils.AddServerToDB;
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.guild.GuildCreateEvent;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.Member;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import reactor.core.publisher.Mono;
@@ -19,6 +21,18 @@ public class GuildJoinListener {
         Guild guild = event.getGuild();
 
         addServerToDB.addServerToDatabase(guild);
+
+        // Add Early Adopter Role - quick n dirty because I'll remove at release
+
+        if (event.getGuild().getId().asString().equals("931389256180580433")) {
+            for (Member guildMember : guild.getMembers().collectList().block()) {
+                for (Guild botGuild : event.getClient().getGuilds().collectList().block()) {
+                    if (botGuild.getOwner().block().equals(guildMember)) {
+                        guildMember.addRole(Snowflake.of("931389913503506502")).block();
+                    }
+                }
+            }
+        }
 
         return Mono.empty();
     }
