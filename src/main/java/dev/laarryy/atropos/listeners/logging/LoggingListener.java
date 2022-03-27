b0ad70c9b3e154e525fafd55cbb4f28f83c83585
emilyy-dev <35617540+emilyy-dev@users.noreply.github.com>
2022-03-27 20:08:24 -0300

    Convert `LogExecutor::logTextDelete` and adjacent methods

--- src/main/java/dev/laarryy/atropos/listeners/logging/LoggingListener.java
+++ src/main/java/dev/laarryy/atropos/listeners/logging/LoggingListener.java
@@ -381,17 +381,9 @@ public final class LoggingListener {
 
     @EventListener
     public Mono<Void> on(TextChannelDeleteEvent event) {
-        wait(2000);
-        Guild guild = event.getChannel().getGuild().block();
-        if (guild == null) return Mono.empty();
-        getLogChannel(guild, "guild")
-                .doOnSuccess(textChannel -> {
-                    if (textChannel != null) {
-                        LogExecutor.logTextDelete(event, textChannel);
-                    }
-                })
-                .subscribe();
-        return Mono.empty();
+        return event.getChannel().getGuild()
+            .flatMap(guild -> getLogChannel(guild, "guild"))
+            .flatMap(channel -> LogExecutor.logTextDelete(event, channel));
     }
 
     @EventListener
--- src/main/java/dev/laarryy/atropos/utils/LogExecutor.java
+++ src/main/java/dev/laarryy/atropos/utils/LogExecutor.java
@@ -1174,29 +1174,31 @@ public final class LogExecutor {
         return responsibleUserId;
     }
 
-    public static void logTextDelete(TextChannelDeleteEvent event, TextChannel logChannel) {
-        AuditLogEntry channelDelete = event.getChannel().getGuild().block().getAuditLog().withActionType(ActionType.CHANNEL_DELETE)
-                .map(AuditLogPart::getEntries)
-                .flatMap(Flux::fromIterable)
-                .filter(auditLogEntry -> auditLogEntry.getResponsibleUser().isPresent())
-                .next()
-                .block();
+    public static Mono<Void> logTextDelete(TextChannelDeleteEvent event, TextChannel logChannel) {
+        final TextChannel channel = event.getChannel();
+        return channel.getGuild()
+            .map(Guild::getAuditLog)
+            .flatMapMany(auditLog -> auditLog.withActionType(ActionType.CHANNEL_DELETE))
+            .flatMapIterable(AuditLogPart::getEntries)
+            .filter(entry -> entry.getResponsibleUser().isPresent())
+            .next()
+            .flatMap(textDelete -> {
+                String responsibleUserId = getAuditResponsibleUser(textDelete);
 
-        String responsibleUserId = getAuditResponsibleUser(channelDelete);
+                long channelId = channel.getId().asLong();
+                String name = channel.getName();
+                String channelDescriptor = "`%d`:`%s`:%s".formatted(channelId, name, channel.getMention());
 
-        long channelId = event.getChannel().getId().asLong();
-        String name = event.getChannel().getName();
-        String channel = "`" + channelId + "`:`" + name + "`:<#" + channelId + ">";
+                EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                    .title(EmojiManager.getTextChannel() + " Text Channel Deleted")
+                    .color(Color.JAZZBERRY_JAM)
+                    .addField("Channel", channelDescriptor, false)
+                    .addField("Deleted By", responsibleUserId, false)
+                    .timestamp(Instant.now())
+                    .build();
 
-        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-                .title(EmojiManager.getTextChannel() + " Text Channel Deleted")
-                .color(Color.JAZZBERRY_JAM)
-                .addField("Channel", channel, false)
-                .addField("Deleted By", responsibleUserId, false)
-                .timestamp(Instant.now())
-                .build();
-
-        logChannel.createMessage(embed).block();
+                return logChannel.createMessage(embed);
+            }).then();
     }
 
     public static void logTextUpdate(TextChannelUpdateEvent event, TextChannel logChannel) {
