c743f496066df6b39ee246f91e0a27d6c09445bd
Laarryy <hey@laarryy.dev>
2022-05-06 01:18:55 -0600

    Reactivize RemoveSinceCommand

--- src/main/java/dev/laarryy/atropos/commands/raid/RemoveSinceCommand.java
+++ src/main/java/dev/laarryy/atropos/commands/raid/RemoveSinceCommand.java
@@ -2,6 +2,11 @@ package dev.laarryy.atropos.commands.raid;
 
 import dev.laarryy.atropos.commands.Command;
 import dev.laarryy.atropos.config.EmojiManager;
+import dev.laarryy.atropos.exceptions.DurationTooLongException;
+import dev.laarryy.atropos.exceptions.MalformedInputException;
+import dev.laarryy.atropos.exceptions.NoPermissionsException;
+import dev.laarryy.atropos.exceptions.NotFoundException;
+import dev.laarryy.atropos.exceptions.NullServerException;
 import dev.laarryy.atropos.models.guilds.DiscordServer;
 import dev.laarryy.atropos.models.joins.ServerUser;
 import dev.laarryy.atropos.models.users.DiscordUser;
@@ -16,6 +21,8 @@ import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.TextChannel;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.discordjson.json.ApplicationCommandOptionChoiceData;
 import discord4j.discordjson.json.ApplicationCommandOptionData;
@@ -72,152 +79,162 @@ public class RemoveSinceCommand implements Command {
     }
 
     public Mono<Void> execute(ChatInputInteractionEvent event) {
-        if (!CommandChecks.commandChecks(event, request.name())) {
-            return Mono.empty();
-        }
-
-        Guild guild = event.getInteraction().getGuild().block();
-
-        if (event.getOption("type").isEmpty()
-                || event.getOption("duration").isEmpty()
-                || event.getOption("type").get().getValue().isEmpty()
-                || event.getOption("duration").get().getValue().isEmpty()) {
-            Notifier.notifyCommandUserOfError(event, "malformedInput");
-            AuditLogger.addCommandToDB(event, false);
-            return Mono.empty();
-        }
-
-        DatabaseLoader.openConnectionIfClosed();
-        DiscordServer discordServer = DiscordServer.findFirst("server_id = ?", guild.getId().asLong());
-        if (discordServer == null) {
-            Notifier.notifyCommandUserOfError(event, "nullServer");
-            DatabaseLoader.closeConnectionIfOpen();
-            return Mono.empty();
-        }
-
-        String type = event.getOption("type").get().getValue().get().asString();
-
-        String durationInput = event.getOption("duration").get().getValue().get().asString();
-
-        Duration duration = DurationParser.parseDuration(durationInput);
-
-        if (duration.toDays() > 2) {
-            Notifier.notifyCommandUserOfError(event, "durationTooLong");
-            AuditLogger.addCommandToDB(event, false);
-            DatabaseLoader.closeConnectionIfOpen();
-            return Mono.empty();
-        }
-
-        Instant startPoint = Instant.now().minus(duration);
-
-        List<ServerUser> serverUserList = ServerUser.find("server_id = ? and date > ?", discordServer.getServerId(), startPoint.toEpochMilli());
-
-        if (serverUserList == null || serverUserList.isEmpty()) {
-            Notifier.notifyCommandUserOfError(event, "404");
-            DatabaseLoader.closeConnectionIfOpen();
-            return Mono.empty();
-        }
-
-        event.deferReply().block();
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("```\n");
-
-        if (type.equals("ban")) {
-            Flux.fromIterable(serverUserList)
-                    .map(serverUser -> this.banUsers(serverUser, guild, event.getInteraction().getMember().get()))
-                    .filter(string -> !string.equals("none"))
-                    .map(string -> sb.append(string).append(" "))
-                        .subscribe(stb -> {
-                        sb.append("```");
-
-                        String affectedUsers = sb.toString();
-
-                        if (affectedUsers.length() >= 3980) {
-                            affectedUsers = affectedUsers.substring(0, 4000) + "...```\n[Content too long, has been limited]";
+        return Mono.from(CommandChecks.commandChecks(event, request.name()))
+                .flatMap(aBoolean -> {
+                    if (!aBoolean) {
+                        return Mono.error(new NoPermissionsException("No Permission"));
+                    }
+                    return Mono.from(event.getInteraction().getGuild()).publishOn(Schedulers.boundedElastic()).publishOn(Schedulers.boundedElastic()).flatMap(guild -> {
+                        if (event.getOption("type").isEmpty()
+                                || event.getOption("duration").isEmpty()
+                                || event.getOption("type").get().getValue().isEmpty()
+                                || event.getOption("duration").get().getValue().isEmpty()) {
+                            return Mono.error(new MalformedInputException("Malformed Input"));
                         }
 
-                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-                                .title(EmojiManager.getUserBan() + " Success")
-                                .color(Color.SEA_GREEN)
-                                .description("**Affected Users**\n" + affectedUsers)
-                                .timestamp(Instant.now())
-                                .build();
-
-                        Notifier.replyDeferredInteraction(event, embed);
-                    });
-        }
-
-        if (type.equals("kick")) {
-            Flux.fromIterable(serverUserList)
-                    .map(serverUser -> this.kickUsers(serverUser, guild, event.getInteraction().getMember().get()))
-                    .filter(string -> !string.equals("none"))
-                    .map(string -> sb.append(string).append(" "))
-                        .subscribe(stb -> {
-                        sb.append("```");
-
-                        String affectedUsers = sb.toString();
-
-                        if (affectedUsers.length() >= 3980) {
-                            affectedUsers = affectedUsers.substring(0, 4000) + "...```\n[Content too long, has been limited]";
+                        DatabaseLoader.openConnectionIfClosed();
+                        DiscordServer discordServer = DiscordServer.findFirst("server_id = ?", guild.getId().asLong());
+                        if (discordServer == null) {
+                            DatabaseLoader.closeConnectionIfOpen();
+                            return Mono.error(new NullServerException("Null Server"));
                         }
 
-                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
-                                .title(EmojiManager.getUserKick() + " Success")
-                                .color(Color.SEA_GREEN)
-                                .description("**Affected Users**\n" + affectedUsers)
-                                .timestamp(Instant.now())
-                                .build();
+                        String type = event.getOption("type").get().getValue().get().asString();
 
-                        Notifier.replyDeferredInteraction(event, embed);
+                        String durationInput = event.getOption("duration").get().getValue().get().asString();
+
+                        Duration duration = DurationParser.parseDuration(durationInput);
+
+                        if (duration.toDays() > 2) {
+                            AuditLogger.addCommandToDB(event, false);
+                            DatabaseLoader.closeConnectionIfOpen();
+                            return Mono.error(new DurationTooLongException("Duration Too Long"));
+                        }
+
+                        Instant startPoint = Instant.now().minus(duration);
+
+                        List<ServerUser> serverUserList = ServerUser.find("server_id = ? and date > ?", discordServer.getServerId(), startPoint.toEpochMilli());
+
+                        if (serverUserList == null || serverUserList.isEmpty()) {
+                            DatabaseLoader.closeConnectionIfOpen();
+                            return Mono.error(new NotFoundException("404 Not Found"));
+                        }
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.append("```\n");
+
+                        if (type.equals("ban")) {
+                            return Flux.fromIterable(serverUserList)
+                                    .map(serverUser -> this.banUsers(serverUser, guild, event.getInteraction().getMember().get()))
+                                    .filter(string -> !string.equals("none"))
+                                    .map(string -> sb.append(string).append(" "))
+                                    .flatMap(stb -> {
+                                        sb.append("```");
+
+                                        String affectedUsers = sb.toString();
+
+                                        if (affectedUsers.length() >= 3980) {
+                                            affectedUsers = affectedUsers.substring(0, 4000) + "...```\n[Content too long, has been limited]";
+                                        }
+
+                                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                                                .title(EmojiManager.getUserBan() + " Success")
+                                                .color(Color.SEA_GREEN)
+                                                .description("**Affected Users**\n" + affectedUsers)
+                                                .timestamp(Instant.now())
+                                                .build();
+
+                                        return Notifier.sendResultsEmbed(event, embed);
+                                    }).then();
+                        }
+
+                        if (type.equals("kick")) {
+                            return Flux.fromIterable(serverUserList)
+                                    .map(serverUser -> this.kickUsers(serverUser, guild, event.getInteraction().getMember().get()))
+                                    .filter(stringMono -> !stringMono.equals("none"))
+                                    .map(string -> sb.append(string).append(" "))
+                                    .flatMap(stb -> {
+                                        sb.append("```");
+
+                                        String affectedUsers = sb.toString();
+
+                                        if (affectedUsers.length() >= 3980) {
+                                            affectedUsers = affectedUsers.substring(0, 4000) + "...```\n[Content too long, has been limited]";
+                                        }
+
+                                        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                                                .title(EmojiManager.getUserKick() + " Success")
+                                                .color(Color.SEA_GREEN)
+                                                .description("**Affected Users**\n" + affectedUsers)
+                                                .timestamp(Instant.now())
+                                                .build();
+
+                                        return Notifier.sendResultsEmbed(event, embed);
+                                    }).then();
+                        }
+                        DatabaseLoader.closeConnectionIfOpen();
+                        return Mono.empty();
                     });
-        }
-        DatabaseLoader.closeConnectionIfOpen();
-        return Mono.empty();
+
+
+                });
     }
 
-    private String banUsers(ServerUser serverUser, Guild guild, Member member) {
-        DatabaseLoader.openConnectionIfClosed();
-        DiscordUser discordUser = DiscordUser.findFirst("id = ?", serverUser.getUserId());
-        long userId = discordUser.getUserIdSnowflake();
-        Set<Snowflake> snowflakeSet = guild.getMemberById(Snowflake.of(userId)).block().getRoleIds();
+        private Mono<String> banUsers (ServerUser serverUser, Guild guild, Member member){
+            DatabaseLoader.openConnectionIfClosed();
+            DiscordUser discordUser = DiscordUser.findFirst("id = ?", serverUser.getUserId());
+            long userId = discordUser.getUserIdSnowflake();
 
-        if (guild.getSelfMember().block().getId().asLong() == userId) {
-            return "";
+            return Mono.from(guild.getSelfMember()).flatMap(selfMember -> {
+                if (selfMember.getId().asLong() == userId) {
+                    return Mono.just("");
+                } else {
+                    return Mono.from(guild.getMemberById(Snowflake.of(userId))).flatMap(memberById -> {
+                        if (memberById.isBot()) {
+                            return Mono.just("");
+                        }
+                        else {
+                            Set<Snowflake> snowflakeSet = memberById.getRoleIds();
+                            return Mono.from(member.hasHigherRoles(snowflakeSet)).flatMap(memberHasHigherRoles -> {
+                                if (!snowflakeSet.isEmpty() && !memberHasHigherRoles) {
+                                    return Mono.just("");
+                                } else {
+                                    return Mono.from(guild.ban(Snowflake.of(userId)).withReason("Banned as part of anti-raid measures").withDeleteMessageDays(2))
+                                            .thenReturn(String.valueOf(userId));
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
 
-        if (guild.getMemberById(Snowflake.of(userId)).block().isBot()) {
-            return "";
-        }
+        private Mono<String> kickUsers (ServerUser serverUser, Guild guild, Member member) {
+            DatabaseLoader.openConnectionIfClosed();
+            DiscordUser discordUser = DiscordUser.findFirst("id = ?", serverUser.getUserId());
+            long userId = discordUser.getUserIdSnowflake();
 
-        if (!snowflakeSet.isEmpty() && !member.hasHigherRoles(snowflakeSet).block()) {
-            return "none";
+            return Mono.from(guild.getSelfMember()).flatMap(selfMember -> {
+                if (selfMember.getId().asLong() == userId) {
+                    return Mono.just("");
+                } else {
+                    return Mono.from(guild.getMemberById(Snowflake.of(userId))).flatMap(memberById -> {
+                        if (memberById.isBot()) {
+                            return Mono.just("");
+                        }
+                        else {
+                            Set<Snowflake> snowflakeSet = memberById.getRoleIds();
+                            return Mono.from(member.hasHigherRoles(snowflakeSet)).flatMap(memberHasHigherRoles -> {
+                                if (!snowflakeSet.isEmpty() && !memberHasHigherRoles) {
+                                    return Mono.just("");
+                                } else {
+                                    return Mono.from(guild.kick(Snowflake.of(userId), "Kicked as part of Anti-Raid Measures"))
+                                            .thenReturn(String.valueOf(userId));
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
-
-        guild.ban(Snowflake.of(userId)).withReason("Banned as part of anti-raid measures").withDeleteMessageDays(2).block();
-        return String.valueOf(userId);
     }
-
-    private String kickUsers(ServerUser serverUser, Guild guild, Member member) {
-        DatabaseLoader.openConnectionIfClosed();
-        DiscordUser discordUser = DiscordUser.findFirst("id = ?", serverUser.getUserId());
-        long userId = discordUser.getUserIdSnowflake();
-
-        if (guild.getSelfMember().block().getId().asLong() == userId) {
-            return "";
-        }
-
-        if (guild.getMemberById(Snowflake.of(userId)).block().isBot()) {
-            return "";
-        }
-
-        Set<Snowflake> snowflakeSet = guild.getMemberById(Snowflake.of(userId)).block().getRoleIds();
-
-        if (!snowflakeSet.isEmpty() && !member.hasHigherRoles(snowflakeSet).block()) {
-            return "";
-        }
-
-        guild.kick(Snowflake.of(userId), "Kicked as part of anti-raid measures").block();
-        return String.valueOf(userId);
-    }
-}
