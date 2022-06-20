0be5b6f98b99c60c2a1ad5d426e3c95faf0f6b2b
emilyy-dev <35617540+emilyy-dev@users.noreply.github.com>
2022-06-20 18:27:28 -0300

    Teeny tiny refactoring around AuditLogger

--- src/main/java/dev/laarryy/atropos/utils/AuditLogger.java
+++ src/main/java/dev/laarryy/atropos/utils/AuditLogger.java
@@ -14,7 +14,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
-import java.util.Objects;
 
 public final class AuditLogger {
     private static final Logger logger = LogManager.getLogger(ManualPunishmentEnder.class);
@@ -22,82 +21,81 @@ public final class AuditLogger {
     private AuditLogger() {}
 
     public static Mono<Void> addCommandToDB(ChatInputInteractionEvent event, boolean success) {
-        DatabaseLoader.openConnectionIfClosed();
+        return Mono.defer(() -> {
+            DatabaseLoader.openConnectionIfClosed();
 
-        if (event.getInteraction().getGuildId().isEmpty()) {
-            return Mono.empty();
-        }
+            if (event.getInteraction().getGuildId().isEmpty()) {
+                return Mono.empty();
+            }
 
-        DiscordServer server = DiscordServer.findFirst("server_id = ?", event.getInteraction().getGuildId().get().asLong());
+            DiscordServer server = DiscordServer.findFirst("server_id = ?", event.getInteraction().getGuildId().get().asLong());
 
-        if (server == null) {
-            return Mono.empty();
-        }
+            if (server == null) {
+                return Mono.empty();
+            }
 
-        int serverId = server.getServerId();
+            int serverId = server.getServerId();
 
-        DiscordUser user = DiscordUser.findFirst("user_id_snowflake = ?", event.getInteraction().getUser().getId().asLong());
+            DiscordUser user = DiscordUser.findFirst("user_id_snowflake = ?", event.getInteraction().getUser().getId().asLong());
 
-        if (user == null) {
-            return Mono.empty();
-        }
-        int commandUserId = user.getUserId();
+            if (user == null) {
+                return Mono.empty();
+            }
+            int commandUserId = user.getUserId();
 
-        StringBuffer stringBuffer = new StringBuffer();
-        StringBuilder sb = new StringBuilder();
-
-        stringBuffer.append(event.getCommandName());
-
-        return Flux.fromIterable(event.getOptions())
-                .flatMap(options -> generateOptionString(options, sb))
-                .map(stringBuffer::append)
-                .doOnComplete(() -> {
-                    String commandContent = stringBuffer.toString();
-                    CommandUse commandUse = CommandUse.findOrCreateIt("server_id", serverId, "command_user_id", commandUserId, "command_contents", commandContent, "date", Instant.now().toEpochMilli(), "success", success);
-                    commandUse.save();
-                })
-                .then();
+            return Flux.fromIterable(event.getOptions())
+                    .flatMap(AuditLogger::generateOptionString)
+                    .reduce(event.getCommandName(), String::concat)
+                    .doOnNext(commandContent -> {
+                        CommandUse commandUse = CommandUse.findOrCreateIt("server_id", serverId, "command_user_id", commandUserId, "command_contents", commandContent, "date", Instant.now().toEpochMilli(), "success", success);
+                        commandUse.save();
+                    })
+                    .then();
+        });
     }
 
     public static Mono<Void> addCommandToDB(ButtonInteractionEvent event, String entry, boolean success) {
-        DatabaseLoader.openConnectionIfClosed();
+        return Mono.fromRunnable(() -> {
+            DatabaseLoader.openConnectionIfClosed();
 
-        if (event.getInteraction().getGuildId().isEmpty()) {
-            return Mono.empty();
-        }
+            if (event.getInteraction().getGuildId().isEmpty()) {
+                return;
+            }
 
-        DiscordServer server = DiscordServer.findFirst("server_id = ?", event.getInteraction().getGuildId().get().asLong());
+            DiscordServer server = DiscordServer.findFirst("server_id = ?", event.getInteraction().getGuildId().get().asLong());
 
-        if (server == null) {
-            return Mono.empty();
-        }
+            if (server == null) {
+                return;
+            }
 
-        int serverId = server.getServerId();
+            int serverId = server.getServerId();
 
-        DiscordUser user = DiscordUser.findFirst("user_id_snowflake = ?", event.getInteraction().getUser().getId().asLong());
+            DiscordUser user = DiscordUser.findFirst("user_id_snowflake = ?", event.getInteraction().getUser().getId().asLong());
 
-        if (user == null) {
-            return Mono.empty();
-        }
-        int commandUserId = user.getUserId();
+            if (user == null) {
+                return;
+            }
+            int commandUserId = user.getUserId();
 
-        CommandUse commandUse = CommandUse.findOrCreateIt("server_id", serverId, "command_user_id", commandUserId, "command_contents", entry, "date", Instant.now().toEpochMilli(), "success", success);
-        commandUse.save();
-
-        return Mono.empty();
+            CommandUse commandUse = CommandUse.findOrCreateIt("server_id", serverId, "command_user_id", commandUserId, "command_contents", entry, "date", Instant.now().toEpochMilli(), "success", success);
+            commandUse.save();
+        });
     }
 
     public static Mono<String> generateOptionString(ApplicationCommandInteractionOption option) {
-        return generateOptionString(option, new StringBuilder());
-    }
+        final Mono<String> nameAndValueMono = stringifyOptionValue(option) // returns Mono.just("") if value is not present
+                .map(value -> ' ' + option.getName() + ':' + value);
+        return Flux.fromIterable(option.getOptions())
+                .flatMap(AuditLogger::generateOptionString)
+                .reduce("", String::concat) // make sure there's at least an empty string, otherwise the zip combinator below won't ever be called
+                .zipWith(nameAndValueMono, (nameAndValue, concatenatedChildren) -> nameAndValue + concatenatedChildren);
 
-    public static Mono<String> generateOptionString(ApplicationCommandInteractionOption option, StringBuilder sb) {
-
+/*
         return Flux.fromIterable(option.getOptions())
                 .flatMap(opt -> {
                     if (opt.getOptions().isEmpty()) {
                         if (opt.getValue().isPresent()) {
-                            return AuditLogger.stringifyOptionValue(opt).flatMap(result -> Mono.just(":" + result));
+                            return AuditLogger.stringifyOptionValue(opt).map(result -> ":" + result);
                         } else {
                             return Mono.just("");
                         }
@@ -109,6 +107,7 @@ public final class AuditLogger {
 
                 })
                 .reduce(" " + option.getName(), String::concat);
+*/
 
         /*return Mono.just(sb).flatMap(unused -> {
             if (option.getValue().isEmpty()) {
@@ -153,14 +152,14 @@ public final class AuditLogger {
             return Mono.just("");
         }
 
-        return switch (option.getType().name()) {
-            case "USER" -> option.getValue().get().asUser().flatMap(user -> Mono.just(user.getId().asString()));
-            case "STRING" -> Mono.just(option.getValue().get().asString());
-            case "INTEGER" -> Mono.just(String.valueOf(option.getValue().get().asLong()));
-            case "BOOLEAN" -> Mono.just(String.valueOf(option.getValue().get().asBoolean()));
-            case "CHANNEL" -> option.getValue().get().asChannel().flatMap(channel -> Mono.just(channel.getId().asString()));
-            case "ROLE" -> option.getValue().get().asRole().flatMap(role -> Mono.just(role.getId().asString()));
-            case "MENTIONABLE" -> Mono.just(option.getValue().get().toString());
+        return switch (option.getType()) {
+            case USER -> option.getValue().get().asUser().map(user -> user.getId().asString());
+            case STRING -> Mono.just(option.getValue().get().asString());
+            case INTEGER -> Mono.just(String.valueOf(option.getValue().get().asLong()));
+            case BOOLEAN -> Mono.just(String.valueOf(option.getValue().get().asBoolean()));
+            case CHANNEL -> option.getValue().get().asChannel().map(channel -> channel.getId().asString());
+            case ROLE -> option.getValue().get().asRole().map(role -> role.getId().asString());
+            case MENTIONABLE -> Mono.just(option.getValue().get().toString());
             default -> Mono.just(option.getName());
         };
     }
