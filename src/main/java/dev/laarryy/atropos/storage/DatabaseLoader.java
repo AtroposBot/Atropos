d4f148ecbce745a03f10cf10e01e18ca760bf2a1
Larry <hey@laarryy.dev>
2025-01-13 15:58:17 -0800

    update deps

--- src/main/java/dev/laarryy/atropos/storage/DatabaseLoader.java
+++ src/main/java/dev/laarryy/atropos/storage/DatabaseLoader.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javalite.activejdbc.Base;
 
-
 public class DatabaseLoader {
 
     private static final Logger logger = LogManager.getLogger(DatabaseLoader.class);
